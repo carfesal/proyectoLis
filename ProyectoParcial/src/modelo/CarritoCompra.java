3e363457d47c47d979e7bd4d621159873957c4ba
Carlos Salazar Moreira <56647144+carfesal@users.noreply.github.com>
2021-07-17 08:50:41 -0500

    Add files via upload

---
+++ ProyectoParcial/src/modelo/CarritoCompra.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author chard
+ */
+public class CarritoCompra {
+    private List<Producto> productos;
+    private double total;
+
+    public CarritoCompra(double total) {
+        this.total = total;
+        this.productos = new ArrayList();
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
+    
+}
---
+++ ProyectoParcial/src/modelo/Producto.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+
+
+/**
+ *
+ * @author chard
+ */
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private String descripcion; 
+    private double precio;
+    private int stock; 
+    private String imagen;
+    
+    
+    public Producto(int codigo, String nombre, String descripcion, double precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+   
+    
+    
+}
